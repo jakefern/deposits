ac6bfdf5df4eaf72af951d2eda29c6a54b3f890a
jakefern <35747562+jakefern@users.noreply.github.com>
2018-01-29 14:29:21 -0800

    Add files via upload

---
+++ NewMath.java
@@ -0,0 +1,27 @@
+/**
+
+* @author Jake Fernandez
+*<p> This is a comment. </p>
+
+
+
+*/
+public class NewMath {
+	public static void main(String[] args) {
+		short s = 15;
+		long l = 8;
+		double d = 9;
+		System.out.print("15 - 8 = ");
+		System.out.println(s - l);
+		System.out.print("8 - 9 = ");
+		System.out.println(l - d);
+		System.out.print("15 - 9 = ");
+		System.out.println(s - d);
+		System.out.print("15 x 8 = ");
+		System.out.println(s * l);
+		System.out.print("8 x 9 = ");
+		System.out.println(l * d);
+		System.out.print("15 x 9 = ");
+		System.out.println(s * d);
+	}
+}
